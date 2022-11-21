8bfe96db563554780a275ae40bff01f43d630e40
Vikrant Kataria <vik7am@gmail.com>
2022-11-21 12:11:38 +0530

    alpha build 1

--- Connect4.cpp
+++ Connect4.cpp
@@ -6,7 +6,9 @@ class Connect4Board{
     const static int COLUMN_SIZE = 7;
     char board[ROW_SIZE][COLUMN_SIZE] = {'\0'};
     int columnSize[COLUMN_SIZE] = {0};
+    
     public:
+    bool gameOver = false;
     void resetBoard(){
         for(int i=0; i<ROW_SIZE; i++)
             for(int j=0; j<COLUMN_SIZE; j++)
@@ -19,6 +21,36 @@ class Connect4Board{
         int row = ROW_SIZE - filledBalls - 1;
         board[row][column] = player;
         columnSize[column]++;
+        for(int i=0; i<7; i++)
+            if(checkBall(player, row, column, i))
+                gameOver = true;
+        return true;
+    }
+    bool checkBall(char player, int row, int column, int direction){
+        int i = row;
+        int j = column;
+        switch(direction){
+            case 0: i-=3; j+=3;break;
+            case 1: j+=3;break;
+            case 2: i+=3; j+=3;break;
+            case 3: i+=3;break;
+            case 4: i+=3; j-=3;break;
+            case 5: j-=3;break;
+            case 6: i-=3; j-=3;break;
+        }
+        if(i<0 || i>=ROW_SIZE || j<0 || j>=COLUMN_SIZE)
+            return false;
+        if(board[row][column] != board[i][j])
+            return false;
+        switch(direction){
+            case 0: if(board[row][column] != board[row-1][column+1] || board[row][column] != board[row-2][column+2]) return false;break;
+            case 1: if(board[row][column] != board[row][column+1] || board[row][column] != board[row][column+2]) return false;break;
+            case 2: if(board[row][column] != board[row+1][column+1] || board[row][column] != board[row+2][column+2]) return false;break;
+            case 3: if(board[row][column] != board[row+1][column] || board[row][column] != board[row+2][column]) return false;break;
+            case 4: if(board[row][column] != board[row+1][column-1] || board[row][column] != board[row+2][column-2]) return false;break;
+            case 5: if(board[row][column] != board[row][column-1] || board[row][column] != board[row][column-2]) return false;break;
+            case 6: if(board[row][column] != board[row-1][column-1] || board[row][column] != board[row-2][column-2]) return false;break;
+        }
         return true;
     }
     void displayBoard(){
@@ -42,24 +74,48 @@ class GameManager{
         char input;
         int column;
         bool flag1;
+        int turn = 0;
         game.displayBoard();
-        for(int i=0; i<6; i++){
+        while(turn < 42){
             if(player1Turn){
                 cout << "Player 1(R) Turn: ";
                 cin >> column;
-                flag1 = game.dropBall('R', column-1);
+                if(column>0 && column <= 7)
+                    flag1 = game.dropBall('R', column-1);
+                else{
+                    cout << "Invalid column number" << endl;
+                    continue;
+                }
             }
             else{
                 cout << "Player 2(B) Turn: ";
                 cin >> column;
-                flag1 = game.dropBall('B', column-1);
+                if(column>0 && column <= 7)
+                    flag1 = game.dropBall('B', column-1);
+                else{
+                    cout << "Invalid column number" << endl;
+                    continue;
+                }
             }
             if(flag1 == false){
                 cout << "No space to drop ball" << endl;
                 continue;
             }
+            if(game.gameOver){
+                break;
+            }
             game.displayBoard();
             player1Turn = !player1Turn;
+            turn++;
+        }
+        if(turn == 42){
+            cout << "Game Draw" << endl;
+        }
+        else if(player1Turn){
+            cout << "Player 1 Won" << endl;
+        }
+        else{
+            cout << "Player 2 Won" << endl;
         }
     }
     
