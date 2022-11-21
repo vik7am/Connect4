af83d9e597675e2f103ef2a84e34ea93f50edbf8
Vikrant Kataria <vik7am@gmail.com>
2022-11-21 16:13:27 +0530

    code opti 3

--- Connect4.cpp
+++ Connect4.cpp
@@ -6,38 +6,35 @@ enum Player{PLAYER1 = 82, PLAYER2 = 66};
 class Connect4Board{
     const int ROW_SIZE = 6;
     const int COLUMN_SIZE = 7;
-    char board[6][7] = {'\0'};
-    int columnSize[7] = {0};
-    bool gameOver = false;
-    
+    char board[6][7];
+    int availableSpace[7];
+    int rowIndex;
+    int columnIndex;
     public:
-    bool isGameOver(){
-        return gameOver;
-    }
-    bool isBoardColumnFull(int columnIndex){
-        int filledBalls = columnSize[columnIndex];
-        if(filledBalls >= ROW_SIZE)
-            return true;
-        return false;
-    }
     void resetBoard(){
         for(int i=0; i<ROW_SIZE; i++)
             for(int j=0; j<COLUMN_SIZE; j++)
                 board[i][j] = 0;
+        for(int i=0; i<COLUMN_SIZE; i++)
+            availableSpace[i] = ROW_SIZE;
     }
-    void dropBall(char player, int columnIndex){
-        char playerName = (char)player;
-        int filledBalls = columnSize[columnIndex];
-        int rowIndex = ROW_SIZE - filledBalls - 1;
+    bool isBoardColumnFull(){
+        if(availableSpace[columnIndex] > 0)
+            return false;
+        return true;
+    }
+    bool dropBall(char playerName, int column){
+        columnIndex = column -1;
+        if(isBoardColumnFull())
+            return false;
+        rowIndex = availableSpace[columnIndex] - 1;
         board[rowIndex][columnIndex] = playerName;
-        columnSize[columnIndex]++;
-        for(int i=0; i<7; i++)
-            if(checkBall(playerName, rowIndex, columnIndex, i))
-                gameOver = true;
+        availableSpace[columnIndex]--;
+        return true;
     }
-    bool checkBall(char player, int row, int column, int direction){
-        int i = row;
-        int j = column;
+    bool checkBall(char player, int direction){
+        int i = rowIndex;
+        int j = columnIndex;
         switch(direction){
             case 0: i=-1; j=1; break;
             case 1: i=0; j=1; break;
@@ -47,15 +44,14 @@ class Connect4Board{
             case 5: i=0; j=-1; break;
             case 6: i=-1; j=-1; break;
         }
-        if(row+3*i<0 || row+3*i>=ROW_SIZE || column+3*j<0 || column+3*j>=COLUMN_SIZE)
+        if(rowIndex+3*i<0 || rowIndex+3*i>=ROW_SIZE || columnIndex+3*j<0 || columnIndex+3*j>=COLUMN_SIZE)
             return false;
-        if(board[row][column] != board[row+3*i][column+3*j])
+        if(board[rowIndex][columnIndex] != board[rowIndex+3*i][columnIndex+3*j])
             return false;
-        if(board[row][column] != board[row+i][column+j])
+        if(board[rowIndex][columnIndex] != board[rowIndex+i][columnIndex+j])
             return false;
-        if(board[row][column] != board[row+2*i][column+2*j])
+        if(board[rowIndex][columnIndex] != board[rowIndex+2*i][columnIndex+2*j])
             return false;
-        return true;
         return true;
     }
     void displayBoard(){
@@ -69,79 +65,74 @@ class Connect4Board{
             cout << "\n|---|---|---|---|---|---|---|" << endl;
         }
     }
-    
 };
 class GameManager{
-    bool player1Turn = true;
-    Player activePlayer = PLAYER1;
+    Player activePlayer;
+    Connect4Board game;
+    bool gameOver;
+    int input;
+    int playerNo;
+    int turn;
     public:
     void playGame(){
-        Connect4Board game;
-        int input;
-        //int column;
-        bool flag1;
-        int turn = 0;
-        int playerNo;
+        activePlayer = PLAYER1;
+        turn = 0;
+        game.resetBoard();
         game.displayBoard();
         while(turn < 42){
-            if(activePlayer == PLAYER1)
-                playerNo = 1;
-            else
-                playerNo = 2;
-            //playerNo = (activePlayer == PLAYER1)? 1: 2;
+            playerNo = (activePlayer == PLAYER1)? 1: 2;
             cout << "Player" << playerNo << "(" << char(activePlayer) << ") Turn: ";
             cin >> input;
             if(input <= 0 || input > 7){
                 cout << "Invalid Input" << endl;
                 continue;
             }
-            if(game.isBoardColumnFull(input-1)){
+            if(game.dropBall(activePlayer, input) == false){
                 cout << "No space to drop ball" << endl;
                 continue;
             }
-            game.dropBall(activePlayer, input-1);
+            for(int i=0; i<7; i++)
+                if(game.checkBall(activePlayer, i))
+                    gameOver = true;
             game.displayBoard();
-            if(game.isGameOver()){
+            if(gameOver)
                 break;
-            }
-            player1Turn = !player1Turn;
+            activePlayer = (activePlayer == PLAYER1)? PLAYER2: PLAYER1;
             turn++;
         }
         if(turn == 42){
-            cout << "Game Draw" << endl;
+            cout << "\nGame Draw\n" << endl;
         }
-        else if(player1Turn){
-            cout << "Player 1 Won" << endl;
+        else if(activePlayer == PLAYER1){
+            cout << "\nPlayer 1 Won\n" << endl;
         }
         else{
-            cout << "Player 2 Won" << endl;
+            cout << "\nPlayer 2 Won\n" << endl;
         }
-    }
-    
-    void showMainMenu(){
-        int playerChoice;
-        bool exitGame;
-        while(exitGame == false){
-            cout << "WELCOME TO CONNECT4" << endl;
-            cout << "1. Start \n2. Exit \nYour Choice ";
-            cin >> playerChoice;
-            if(playerChoice == 1){
-                playGame();
-            }
-            else if(playerChoice ==2){
-                cout << "Thanks for Playing!" << endl;
-                exitGame = true;
-            }
-            else{
-                cout << "Invalid Input Try Again!" << endl;
-            }
-        }
+        cout << "Press 1 to continue ";
+        cin >> input;
     }
 };
 
 int main()
 {
     GameManager gameManager;
-        gameManager.showMainMenu();
+    int playerChoice;
+     bool exitGame;
+    while(exitGame == false){
+        cout << "WELCOME TO CONNECT4" << endl;
+        cout << "1. Start \n2. Exit \nYour Choice ";
+        cin >> playerChoice;
+        if(playerChoice == 1){
+            gameManager.playGame();
+        }
+        else if(playerChoice ==2){
+            cout << "Thanks for Playing!" << endl;
+            exitGame = true;
+        }
+        else{
+            cout << "Invalid Input Try Again!" << endl;
+        }
+    }
     cout << endl;
 }
