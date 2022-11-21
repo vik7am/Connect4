8a560d790f2a6547e332dab9ff91cc19d84823e5
Vikrant Kataria <vik7am@gmail.com>
2022-11-21 13:52:21 +0530

    code opti 2

--- Connect4.cpp
+++ Connect4.cpp
@@ -1,30 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+enum Player{PLAYER1 = 82, PLAYER2 = 66};
+
 class Connect4Board{
-    const static int ROW_SIZE = 6;
-    const static int COLUMN_SIZE = 7;
-    char board[ROW_SIZE][COLUMN_SIZE] = {'\0'};
-    int columnSize[COLUMN_SIZE] = {0};
+    const int ROW_SIZE = 6;
+    const int COLUMN_SIZE = 7;
+    char board[6][7] = {'\0'};
+    int columnSize[7] = {0};
+    bool gameOver = false;
     
     public:
-    bool gameOver = false;
+    bool isGameOver(){
+        return gameOver;
+    }
+    bool isBoardColumnFull(int columnIndex){
+        int filledBalls = columnSize[columnIndex];
+        if(filledBalls >= ROW_SIZE)
+            return true;
+        return false;
+    }
     void resetBoard(){
         for(int i=0; i<ROW_SIZE; i++)
             for(int j=0; j<COLUMN_SIZE; j++)
                 board[i][j] = 0;
     }
-    bool dropBall(char player, int column){
-        int filledBalls = columnSize[column];
-        if(filledBalls >= ROW_SIZE)
-            return false;
-        int row = ROW_SIZE - filledBalls - 1;
-        board[row][column] = player;
-        columnSize[column]++;
+    void dropBall(char player, int columnIndex){
+        char playerName = (char)player;
+        int filledBalls = columnSize[columnIndex];
+        int rowIndex = ROW_SIZE - filledBalls - 1;
+        board[rowIndex][columnIndex] = playerName;
+        columnSize[columnIndex]++;
         for(int i=0; i<7; i++)
-            if(checkBall(player, row, column, i))
+            if(checkBall(playerName, rowIndex, columnIndex, i))
                 gameOver = true;
-        return true;
     }
     bool checkBall(char player, int row, int column, int direction){
         int i = row;
@@ -64,41 +73,35 @@ class Connect4Board{
 };
 class GameManager{
     bool player1Turn = true;
+    Player activePlayer = PLAYER1;
     public:
     void playGame(){
         Connect4Board game;
-        char input;
-        int column;
+        int input;
+        //int column;
         bool flag1;
         int turn = 0;
+        int playerNo;
         game.displayBoard();
         while(turn < 42){
-            if(player1Turn){
-                cout << "Player 1(R) Turn: ";
-                cin >> column;
-                if(column>0 && column <= 7)
-                    flag1 = game.dropBall('R', column-1);
-                else{
-                    cout << "Invalid column number" << endl;
-                    continue;
-                }
+            if(activePlayer == PLAYER1)
+                playerNo = 1;
+            else
+                playerNo = 2;
+            //playerNo = (activePlayer == PLAYER1)? 1: 2;
+            cout << "Player" << playerNo << "(" << char(activePlayer) << ") Turn: ";
+            cin >> input;
+            if(input <= 0 || input > 7){
+                cout << "Invalid Input" << endl;
+                continue;
             }
-            else{
-                cout << "Player 2(B) Turn: ";
-                cin >> column;
-                if(column>0 && column <= 7)
-                    flag1 = game.dropBall('B', column-1);
-                else{
-                    cout << "Invalid column number" << endl;
-                    continue;
-                }
-            }
-            if(flag1 == false){
+            if(game.isBoardColumnFull(input-1)){
                 cout << "No space to drop ball" << endl;
                 continue;
             }
+            game.dropBall(activePlayer, input-1);
             game.displayBoard();
-            if(game.gameOver){
+            if(game.isGameOver()){
                 break;
             }
             player1Turn = !player1Turn;
